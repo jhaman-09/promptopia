95f939068ca8762b383353180b1754f546a6fe9d
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-12-09 12:19:47 +0530

    search with keyword like tag, username and content added 💙💙

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
+import { FaSearch } from "react-icons/fa";
 
 const PromptCardList = ({ posts, handleTagClick }) => {
   return posts.length !== 0 ? (
@@ -45,7 +46,8 @@ const Feed = () => {
       const filtered = posts.filter(
         (post) =>
           post.tag.toLowerCase().includes(text.toLowerCase()) ||
-          post.prompt.toLowerCase().includes(text.toLowerCase())
+          post.prompt.toLowerCase().includes(text.toLowerCase()) ||
+          post.creator.username.toLowerCase().includes(text.toLowerCase())
       );
       setFilteredPosts(filtered);
     }
@@ -57,17 +59,22 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
+  console.log(posts);
+
   return (
     <section className="feed">
       <form className="relative w-full text-center items-center search_input">
-        <input
-          type="text"
-          placeholder="🔍 Search for a tag or a username"
-          value={searchText}
-          onChange={handleSearchChange}
-          required
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-orange-500"
-        />
+        <div className="w-full flex flex-row items-center border border-gray-300 rounded-md focus-within:ring-2 focus-within:ring-orange-500">
+          <FaSearch className="text-gray-400 ml-2" />
+          <input
+            type="text"
+            placeholder="Search for a tag or a username"
+            value={searchText}
+            onChange={handleSearchChange}
+            required
+            className="w-full pl-2 py-1 text-lg focus:outline-none"
+          />
+        </div>
       </form>
 
       <PromptCardList posts={filteredPosts} handleTagClick={handleTagClick} />
